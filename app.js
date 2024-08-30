4168163d2c43bfae3a68c13204d9c5344bc2e0e2
[Mohammed Alfez] <[alfez637@gmail.com]>
2024-08-30 19:02:52 +0530

    Added result and score function

--- app.js
+++ app.js
@@ -6,29 +6,51 @@ stone.addEventListener("click", () => opponent('stone'));
 paper.addEventListener("click", () => opponent('paper'));
 scissor.addEventListener("click", () => opponent('scissor'));
 
+let score = {
+    win: 0,
+    loss: 0,
+    tie: 0,
+}
+
+let scoreShow = () =>{
+    document.querySelector("#score").innerHTML = 
+    `Win: ${score.win} Loss: ${score.loss} Tie: ${score.tie}`;
+}
+
+let resultShow = () => {
+    document.querySelector("#result").innerHTML = result;
+}
+
+let result = '';
+
 const opponent = (playerChoice) => {
     let randomNumber = Math.random() * 3;
     let computerChoice;
 
-    if(randomNumber >= 0 && randomNumber < 1){
+    if (randomNumber >= 0 && randomNumber < 1) {
         computerChoice = 'stone';
-    }
-    else if(randomNumber >= 1 && randomNumber < 2){
+    } else if (randomNumber >= 1 && randomNumber < 2) {
         computerChoice = 'paper';
-    }
-    else{
+    } else {
         computerChoice = 'scissor';
     }
 
-    if(playerChoice === computerChoice){
-        alert("It's a tie");
-    } else if(
+    if (playerChoice === computerChoice) {
+        score.tie++;
+        result = `Computer chose ${computerChoice}  It's a tie!!`;
+    } else if (
         (playerChoice === 'stone' && computerChoice === 'scissor') ||
         (playerChoice === 'scissor' && computerChoice === 'paper') ||
         (playerChoice === 'paper' && computerChoice === 'stone')
-    ){
-        alert(`Computer chose ${computerChoice} and You won!!`);
+    ) {
+        score.win++;
+        result = `Computer chose ${computerChoice} You won!! `;
     } else {
-        alert(`Computer chose ${computerChoice} and Computer won`);
+        score.loss++;
+        result = `Computer chose ${computerChoice}  Computer won!!      
+      `;
     }
+    
+    resultShow(); 
+    scoreShow();
 }
