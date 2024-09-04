9ad5abd1b0bc272d21fed539d5180de8b09dc42e
[Mohammed Alfez] <[alfez637@gmail.com]>
2024-09-04 11:12:12 +0530

    Game updated

--- app.js
+++ app.js
@@ -1,56 +1,71 @@
 let stone = document.querySelector("#stone");
 let paper = document.querySelector("#paper");
 let scissor = document.querySelector("#scissor");
+let reset_Btn = document.querySelector("#resetBtn");
 
-stone.addEventListener("click", () => opponent('stone'));
-paper.addEventListener("click", () => opponent('paper'));
-scissor.addEventListener("click", () => opponent('scissor'));
+stone.addEventListener("click", () => opponent("stone"));
+paper.addEventListener("click", () => opponent("paper"));
+scissor.addEventListener("click", () => opponent("scissor"));
 
-let score = {
-    win: 0,
-    loss: 0,
-    tie: 0,
+reset_Btn.addEventListener("click", () =>
+  { localStorage.clear(),
+  score = {win: 0, loss: 0, tie: 0},
+  scoreShow(),
+  document.querySelector(
+    "#result"
+  ).innerHTML = ""
 }
+);
 
-let scoreShow = () =>{
-    document.querySelector("#score").innerHTML = 
-    `Win: ${score.win} Loss: ${score.loss} Tie: ${score.tie}`;
-}
+let scoreStr = localStorage.getItem('score');
+let score = JSON.parse(scoreStr) || {
+  win: 0, 
+  loss: 0,
+  tie: 0,
+};
+
+let scoreShow = () => {
+  document.querySelector(
+    "#score"
+  ).innerHTML = `Win: ${score.win} Loss: ${score.loss} Tie: ${score.tie}`;
+};
 
 let resultShow = () => {
-    document.querySelector("#result").innerHTML = result;
-}
+  localStorage.setItem('score', JSON.stringify(score));
+  document.querySelector("#result").innerHTML = result;
+};
 
-let result = '';
+let result = "";
 
 const opponent = (playerChoice) => {
-    let randomNumber = Math.random() * 3;
-    let computerChoice;
+  let randomNumber = Math.random() * 3;
+  let computerChoice;
 
-    if (randomNumber >= 0 && randomNumber < 1) {
-        computerChoice = 'stone';
-    } else if (randomNumber >= 1 && randomNumber < 2) {
-        computerChoice = 'paper';
-    } else {
-        computerChoice = 'scissor';
-    }
+  if (randomNumber >= 0 && randomNumber < 1) {
+    computerChoice = "stone";
+  } else if (randomNumber >= 1 && randomNumber < 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissor";
+  }
 
-    if (playerChoice === computerChoice) {
-        score.tie++;
-        result = `Computer chose ${computerChoice}  It's a tie!!`;
-    } else if (
-        (playerChoice === 'stone' && computerChoice === 'scissor') ||
-        (playerChoice === 'scissor' && computerChoice === 'paper') ||
-        (playerChoice === 'paper' && computerChoice === 'stone')
-    ) {
-        score.win++;
-        result = `Computer chose ${computerChoice} You won!! `;
-    } else {
-        score.loss++;
-        result = `Computer chose ${computerChoice}  Computer won!!      
+  if (playerChoice === computerChoice) {
+    score.tie++;
+    result = `Computer chose ${computerChoice}  It's a tie!!`;
+  } else if (
+    (playerChoice === "stone" && computerChoice === "scissor") ||
+    (playerChoice === "scissor" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "stone")
+  ) {
+    score.win++;
+    result = `Computer chose ${computerChoice} You won!! `;
+  } else {
+    score.loss++;
+    result = `Computer chose ${computerChoice}  Computer won!!      
       `;
-    }
-    
-    resultShow(); 
-    scoreShow();
-}
+  }
+
+  resultShow();
+  scoreShow();
+};
+
